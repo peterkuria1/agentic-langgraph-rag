e771c6b8a82ac870e79ff52754111a58c0b6f2b0
Peter Kuria <soft.av.networks@gmail.com>
2024-10-01 18:42:14 +0300

    update main module

--- app/backend/main.py
+++ app/backend/main.py
@@ -4,75 +4,73 @@ import numpy as np
 
 import openai
 import time
+import os
+
+from core.utils import get_session, sidebar
+from streamlit.logger import get_logger
+
+from core.retriever import run_llm
+from core.loader import load_data, prepare_chunk
+from core.rag import loader, vector_stores, retrieval
+
+from core.vector_stores import create_vector_stores
+
+logger = get_logger(__name__)
+
+st.set_page_config(
+    page_title="AgenticRAG",
+    initial_sidebar_state="expanded",
+    menu_items={"About": "# *Agentic Local RAG* with  Gdrants, postgres and Ollama"},
+)
+
+st.header("Agentic GraphRAG with Qdrant, Ollama and Postres")
+
+sidebar()
+
+uploaded_file = st.file_uploader("Please upload a file", type=("pdf", "docx", "txt", "md",))
 
 
-# st.title('Uber pickups in NYC')
+def setup():
+    if not uploaded_file:
+        st.stop()
 
-def main():
+    if uploaded_file:
+        st.toast("Your file has been uploaded!", icon="✅")
+        time.sleep(0.5)
 
-    if 'client' not in st.session_state:
-        # Initialize the client
-        st.session_state.client = openai.OpenAI()
+        with open(uploaded_file.name, mode="wb") as w:
+            w.write(uploaded_file.getvalue())
 
-        st.session_state.file = st.session_state.client.files.create(
-            file=open("songs.txt", "rb"),
-            purpose='assistants'
-        )
+        documents = loader(file=uploaded_file, file_loc=uploaded_file.name)
+        os.remove(uploaded_file.name)
+        if documents is None:
+            st.info("No data found, please upload a valid file")
+            st.stop()
 
-        # Step 1: Create an Assistant
-        st.session_state.assistant = st.session_state.client.beta.assistants.create(
-            name="Customer Service Assistant",
-            instructions="You are a customer support chatbot. Use your knowledge base to best respond to customer queries.",
-            model="gpt-4-1106-preview",
-            file_ids=[st.session_state.file.id],
-            tools=[{"type": "retrieval"}]
-        )
+        elif documents is not None:
+            with st.spinner("Please wait for Indexing docs"):
+                vs_conn = vector_stores(documents=documents)
+                st.toast("File indexing completed", icon="🚀")
+                time.sleep(0.5)
 
-        # Step 2: Create a Thread
-        st.session_state.thread = st.session_state.client.beta.threads.create()
+    return vs_conn
 
-    user_query = st.text_input("Enter your query:", "Tell me about Dance Monkey")
 
-    if st.button('Submit'):
-        # Step 3: Add a Message to a Thread
-        message = st.session_state.client.beta.threads.messages.create(
-            thread_id=st.session_state.thread.id,
-            role="user",
-            content=user_query
-        )
+def welcome_message():
+    # Initialize chat history
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Chat with your document, any question?"}
+    ]
+    with st.chat_message("assistant"):
+        for msg in st.session_state.messages:
+            st.markdown(msg["content"])
+    # Create session id to store chat history in redis
+    st.session_state.session_id = get_session()
 
-        # Step 4: Run the Assistant
-        run = st.session_state.client.beta.threads.runs.create(
-            thread_id=st.session_state.thread.id,
-            assistant_id=st.session_state.assistant.id,
-            instructions="Please address the user as Mervin Praison"
-        )
-
-        while True:
-            # Wait for 5 seconds
-            time.sleep(5)
-
-            # Retrieve the run status
-            run_status = st.session_state.client.beta.threads.runs.retrieve(
-                thread_id=st.session_state.thread.id,
-                run_id=run.id
-            )
-
-            # If run is completed, get messages
-            if run_status.status == 'completed':
-                messages = st.session_state.client.beta.threads.messages.list(
-                    thread_id=st.session_state.thread.id
-                )
-
-                # Loop through messages and print content based on role
-                for msg in messages.data:
-                    role = msg.role
-                    content = msg.content[0].text.value
-                    st.write(f"{role.capitalize()}: {content}")
-                break
-            else:
-                st.write("Waiting for the Assistant to process...")
-                time.sleep(5)
 
 if __name__ == "__main__":
-    main()
+    if "messages" not in st.session_state:
+        vs_conn = setup()
+        welcome_message()
+    if prompt := st.chat_input():
+        rag_chat(prompt, model="llama3")
